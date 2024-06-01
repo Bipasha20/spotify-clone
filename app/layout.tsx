f2d2ec5fbef12e7f0d2b6cb96740cf247246c563
Bipasha20 <bipashaguha03@gmail.com>
2024-06-01 13:07:00 +0530

    final

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import "./globals.css";
+import { Figtree } from "next/font/google";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
+import getActiveProductsWithPrices from "@/actions/getActiveProductsWithPrices";
+
+import Sidebar from "@/components/Sidebar";
+import ToasterProvider from "@/providers/ToasterProvider";
+import getSongsByUserId from "@/actions/getSongByUserId";
+import Player from "@/components/Player";
+
+const font = Figtree({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Spotify Clone",
+  description: "Listening to music!",
+};
+
+export const revalidate = 0;
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const userSongs = await getSongsByUserId();
+  const products = await getActiveProductsWithPrices();
+
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ToasterProvider />
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider products={products} />
+            <Sidebar songs={userSongs}>{children}</Sidebar>
+            <Player />
+          </UserProvider>
+        </SupabaseProvider>
+      </body>
+    </html>
+  );
+}
---
+++ components/Library.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai";
+
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
+import { Song } from "@/types/myTypes";
+import MediaItem from "./MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
+import useSubscribeModal from "@/hooks/useSubscribeModal";
+
+type Props = {
+  songs: Song[];
+};
+
+const Library = ({ songs }: Props) => {
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const subscribeModal = useSubscribeModal();
+
+  const { user, subscription } = useUser();
+  const onPlay = useOnPlay(songs);
+
+  const onClick = () => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    // #todo check for subscription
+    if (!subscription) {
+      return subscribeModal.onOpen();
+    }
+
+    return uploadModal.onOpen();
+  };
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex items-center justify-between px-5 pt-4">
+        <div className="inline-flex items-center gap-x-2">
+          <TbPlaylist className="text-neutral-400" size={26} />
+          <p className="font-medium text-neutral-400 text-md">Your Library</p>
+        </div>
+        <AiOutlinePlus
+          onClick={onClick}
+          size={20}
+          className="transition cursor-pointer text-neutral-400 hover:text-white"
+        />
+      </div>
+      <div className="flex flex-col px-3 mt-4 gap-y-2">
+        {songs.map((item) => (
+          <MediaItem
+            onClick={(id: string) => onPlay(id)}
+            key={item.id}
+            data={item}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Library;
